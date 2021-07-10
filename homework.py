4730ef027f58419a7fdf6b32d0c8cb53aac756c8
Aleksey Petukhov <ikazman@yandex.ru>
2021-07-10 17:58:26 +0300

    Tweaks in sleep time

--- homework.py
+++ homework.py
@@ -6,7 +6,6 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 logging.basicConfig(
@@ -15,12 +14,10 @@ logging.basicConfig(
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
 
-
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -38,11 +35,11 @@ def parse_homework_status(homework):
         current_status = homework.get('status')
 
         if current_status is None or current_status not in statuses:
-            verdict = 'Работа не найдена или статус не известен.'
+            verdict = 'Работа не найдена или статус неизвестен.'
         else:
             verdict = statuses[current_status]
 
-        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'"{homework_name}":\n\n{verdict}'
 
     except Exception as e:
         error_message = f'Бот упал с ошибкой: {e}'
@@ -51,6 +48,9 @@ def parse_homework_status(homework):
 
 def get_homeworks(current_timestamp):
     """Получаем данные по всем домашним работам."""
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+
     header = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     params = {'from_date': current_timestamp}
 
@@ -60,8 +60,13 @@ def get_homeworks(current_timestamp):
                                          params=params)
         return homework_statuses.json()
 
+    except ValueError as e:
+        logging.exception(f'Ошибка значения {e}')
+        return {}
+
     except requests.exceptions.RequestException as e:
         logging.exception(f'Ошибка запроса {e}')
+        return {}
 
 
 def send_message(message):
@@ -80,13 +85,11 @@ def main():
         try:
             homeworks = get_homeworks(current_timestamp)
             new_homework = homeworks.get('homeworks')
-            if not current_timestamp:
-                current_timestamp = int(time.time())
             if new_homework:
                 current_status = parse_homework_status(new_homework[0])
                 send_message(current_status)
-            time.sleep(300)
             current_timestamp = homeworks.get('current_date')
+            time.sleep(300)
 
         except Exception as e:
             error_message = f'Бот упал с ошибкой: {e}'
